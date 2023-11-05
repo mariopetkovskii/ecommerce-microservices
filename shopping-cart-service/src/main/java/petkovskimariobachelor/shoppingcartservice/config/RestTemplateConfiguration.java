c1f3c6177424ee2d59a04fe9459e3876eeae366d
Mario Petkovski <mario.petkovski@teaming.inteligenta.io>
2023-11-06 00:31:26 +0100

    update rest template config beans

--- shopping-cart-service/src/main/java/petkovskimariobachelor/shoppingcartservice/config/RestTemplateConfiguration.java
+++ shopping-cart-service/src/main/java/petkovskimariobachelor/shoppingcartservice/config/RestTemplateConfiguration.java
@@ -1,20 +1,17 @@
 package petkovskimariobachelor.shoppingcartservice.config;
 
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfiguration {
-
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(){
-        RestTemplate restTemplate = new RestTemplate();
-        var requestFactory = new SimpleClientHttpRequestFactory();
-        restTemplate.setRequestFactory(requestFactory);
-        return restTemplate;
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
     }
 }
